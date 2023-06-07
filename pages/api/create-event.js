3c4c33f1230296b04c1ff3a3a56c57c88f1ac8b5
Mary Gathoni <maryg.kahiga@gmail.com>
2023-06-08 01:07:25 +0300

    feat: Add endpoint that returns event documents

--- pages/api/create-event.js
+++ pages/api/create-event.js
@@ -12,42 +12,75 @@ export default async (req, res) => {
   const databaseID = process.env.APPWRITE_DATABASE_ID;
   const eventCollectionID = process.env.EVENT_COLLECTION_ID;
   const sessionCollectionId = process.env.SESSION_COLLECTION_ID;
-
-  try {
-    // Create event
-    const event = await databases.createDocument(
-      databaseID,
-      eventCollectionID,
-      ID.unique(),
-      eventData
-    );
-    if (event.error) throw Error(event.error);
-    const sessionIds = [];
-    // // Create session
-    for (const session of sessionData) {
-      session.eventId = event.$id; // Add the event ID to each session
-      const sessionDoc = await databases.createDocument(
-        databaseID,
-        sessionCollectionId,
-        ID.unique(),
-        session
-      );
-      sessionIds.push(sessionDoc.$id); // Add session ID to the array (to be added to the event collection to denormalize it)
-    }
-
-    await databases.updateDocument(
-      event.$databaseId,
-      event.$collectionId,
-      event.$id,
-      {
-        sessions: sessionIds,
+  if (req.method.toUpperCase() == "GET") {
+    const { id } = req.query;
+    try {
+      // If id is pecified in query param, return the document associated with that id
+      if (id) {
+        const document = await databases.getDocument(
+          databaseID,
+          eventCollectionID,
+          id
+        );
+        return res.status(200).json({
+          data: document,
+          error: null,
+        });
       }
-    );
+      // Else return all documents
+      const results = await databases.listDocuments(
+        databaseID,
+        eventCollectionID
+      );
+      res.status(200).json({
+        data: results.documents,
+        error: null,
+      });
+    } catch (error) {
+      res.status(500).json({
+        data: null,
+        error: error.message,
+      });
+    }
+  }
 
-    res
-      .status(201)
-      .json({ success: true, message: "Event created successfully" });
-  } catch (error) {
-    res.status(401).json({ success: false, error: error.toString() });
+  if (req.method.toUpperCase() == "POST") {
+    try {
+      // Create event
+      const event = await databases.createDocument(
+        databaseID,
+        eventCollectionID,
+        ID.unique(),
+        eventData
+      );
+      if (event.error) throw Error(event.error);
+      const sessionIds = [];
+      // // Create session
+      for (const session of sessionData) {
+        session.eventId = event.$id; // Add the event ID to each session
+        const sessionDoc = await databases.createDocument(
+          databaseID,
+          sessionCollectionId,
+          ID.unique(),
+          session
+        );
+        sessionIds.push(sessionDoc.$id); // Add session ID to the array (to be added to the event collection to denormalize it)
+      }
+
+      await databases.updateDocument(
+        event.$databaseId,
+        event.$collectionId,
+        event.$id,
+        {
+          sessions: sessionIds,
+        }
+      );
+
+      res
+        .status(201)
+        .json({ success: true, message: "Event created successfully" });
+    } catch (error) {
+      res.status(401).json({ success: false, error: error.toString() });
+    }
   }
 };
