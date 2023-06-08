28b1df1d53697d5aa7bcf01c9799a07641b3d4d7
Mary Gathoni <maryg.kahiga@gmail.com>
2023-06-08 04:02:58 +0300

    feat: Add GET endpoint for speakers collection

--- pages/api/edit-speaker.js
+++ pages/api/edit-speaker.js
@@ -12,6 +12,38 @@ export default async (req, res) => {
   const databaseID = process.env.APPWRITE_DATABASE_ID;
   const speakerCollectionId = process.env.SPEAKER_COLLECTION_ID;
 
+  if (req.method.toUpperCase() == "GET") {
+    const { id } = req.query;
+    try {
+      // If id is pecified in query param, return the document associated with that id
+      if (id) {
+        const document = await databases.getDocument(
+          databaseID,
+          speakerCollectionId,
+          id
+        );
+        return res.status(200).json({
+          data: document,
+          error: null,
+        });
+      }
+      // Else return all documents
+      const results = await databases.listDocuments(
+        databaseID,
+        speakerCollectionId
+      );
+      res.status(200).json({
+        data: results.documents,
+        error: null,
+      });
+    } catch (error) {
+      res.status(500).json({
+        data: null,
+        error: error.message,
+      });
+    }
+  }
+
   if (req.method.toUpperCase() == "PATCH") {
     try {
       // Update event
