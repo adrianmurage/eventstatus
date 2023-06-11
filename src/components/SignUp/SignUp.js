5286e2cc6a32b7ee45142cdca39d1cb86f70dfe8
Mary Gathoni <maryg.kahiga@gmail.com>
2023-06-11 18:00:38 +0300

    feat: Add signup page

---
+++ src/components/SignUp/SignUp.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { createUser } from "../../utils/db";
+import FormInput from "../FormInput/FormInput";
+
+function SignUp() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [passwordMatch, setPasswordMatch] = useState(true);
+  const [error, seterror] = useState("");
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const newUser = await createUser(user.email, user.password);
+      if (!newUser.id) throw Error();
+    } catch (error) {
+      seterror("An error occured during sign up. Please try again later.");
+    }
+  };
+  const handleChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleConfirmPasswordChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+    setPasswordMatch(e.target.value === user.password);
+  };
+  return (
+    <div>
+      <h1 className="text-2xl">Sign up with your email and password</h1>
+      {error}
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          label="Email"
+          type="email"
+          handler={handleChange}
+          value={user.email}
+          name="email"
+        />
+        <FormInput
+          label="Password"
+          type="password"
+          handler={handleChange}
+          value={user.password}
+          name="password"
+        />
+        <FormInput
+          label="Confirm Password"
+          type="password"
+          handler={handleConfirmPasswordChange}
+          value={user.confirmPassword}
+          name="confirmPassword"
+        />
+        {!passwordMatch && (
+          <small className="mt-1">Passwords do not match.</small>
+        )}
+        <div className="form-control space-y-6 pb-10 pt-5">
+          <button
+            type="submit"
+            className="btn capitalize"
+            onClick={handleSubmit}
+          >
+            Sign Up
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default SignUp;
---
+++ src/pages/auth/signup.js
@@ -0,0 +1,9 @@
+import SignUp from "../../components/SignUp/signup";
+
+export default function signup() {
+  return (
+    <div className="container max-w-xs mx-auto pt-6">
+      <SignUp />
+    </div>
+  );
+}
