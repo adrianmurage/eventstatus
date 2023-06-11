e6f6191c0d7e21035932988d9cc42b54e65c055e
Mary Gathoni <maryg.kahiga@gmail.com>
2023-06-11 15:52:43 +0300

    chore: Replace API calls with calls to db helpers

--- src/pages/events/[id].js
+++ src/pages/events/[id].js
@@ -1,39 +1,38 @@
 import Sessions from "../../components/Sessions/Sessions";
-import { getCapitalizedString, getDate } from "../../utils";
+import { getAllEvents, getAllSessionsInEvent, getEvent } from "../../utils/db";
+import { getCapitalizedString, getDate } from "../../utils/utils";
 
 export default function Page({ event, sessions }) {
-  console.log(process.env.NEXT_PUBLIC_BASE_URL);
   if (!event) {
     return <div>No event was found</div>;
   }
   return (
     <div className="container max-w-xs mx-auto pt-6">
       <section className="mb-4">
-        <h1 className="text-2xl mb-1">Welcome to {event.data.name}</h1>
+        <h1 className="text-2xl mb-1">Welcome to {event.name}</h1>
         <h2 className="text-slate-700 text-lg dark:text-slate-300">
-          {getDate(event.data.date)}
+          {getDate(event.date)}
         </h2>
         <h2 className="text-slate-700 mb-2 dark:text-slate-300">
-          {getCapitalizedString(event.data.venue)}
+          {getCapitalizedString(event.venue)}
         </h2>
       </section>
       <section>
         {!sessions ? (
           <div>This event does not have any sessions</div>
         ) : (
-          <Sessions sessions={sessions.data} />
+          <Sessions sessions={sessions} />
         )}
       </section>
     </div>
   );
 }
 export async function getStaticPaths() {
-  // Call API endpoint to get events
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}api/events`);
-  const events = await res.json();
-
+  // Get all events
+  const events = await getAllEvents();
+  console.log(events);
   // Get the paths we want to prerender based on event ids
-  const paths = events.data.map((event) => ({
+  const paths = events.map((event) => ({
     params: { id: event.$id },
   }));
 
@@ -41,18 +40,11 @@ export async function getStaticPaths() {
   return { paths, fallback: false };
 }
 export async function getStaticProps({ params }) {
-  // Call an API endpoint to get events.
-  const eventRepsponse = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/events?id=${params.id}`
-  );
+  // Get an event
+  const event = await getEvent(params.id);
 
-  // Call an API endpoint to get sessions.
-
-  const event = await eventRepsponse.json();
-  const sessionResponse = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}api/sessions?eventId=${params.id}`
-  );
-  const sessions = await sessionResponse.json();
+  // Get sessions
+  const sessions = await getAllSessionsInEvent(params.id);
 
   return {
     props: {
