89d4ad933f6fc356f3818dfbc1894e4df789a4a7
Mary Gathoni <maryg.kahiga@gmail.com>
2023-06-11 15:51:42 +0300

    chore: Create db helper functions

--- src/utils/appwrite.js
+++ src/utils/appwrite.js
@@ -1,11 +1,19 @@
-import { Client, Databases, ID, Query } from 'appwrite';
+import { Client, DB, Databases, Query } from "appwrite";
 const client = new Client();
 client
-  .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
-  .setProject('647b927f416135cd4ef0'); // Your project ID
+  .setEndpoint("https://cloud.appwrite.io/v1") // Your API Endpoint
+  .setProject("647b927f416135cd4ef0"); // Your project ID
 
 const databases = new Databases(client);
 const databaseID = process.env.APPWRITE_DATABASE_ID;
 const eventCollectionID = process.env.EVENT_COLLECTION_ID;
+const sessionCollectionID = process.env.SESSION_COLLECTION_ID;
 
-export { databases, databaseID, eventCollectionID };
+export {
+  DB,
+  Query,
+  databaseID,
+  databases,
+  eventCollectionID,
+  sessionCollectionID,
+};
