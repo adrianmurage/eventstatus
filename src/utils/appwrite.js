68bf2073903c707982ae2c6cc9648e0224b98e85
Mary Gathoni <maryg.kahiga@gmail.com>
2023-06-12 17:50:37 +0300

    feat: Add function that uploads image to bucket

--- src/utils/appwrite.js
+++ src/utils/appwrite.js
@@ -1,4 +1,4 @@
-import { Account, Client, DB, Databases, ID, Query } from "appwrite";
+import { Account, Client, DB, Databases, ID, Query, Storage } from "appwrite";
 const client = new Client();
 client
   .setEndpoint("https://cloud.appwrite.io/v1") // Your API Endpoint
@@ -6,17 +6,23 @@ client
 
 const databases = new Databases(client);
 const account = new Account(client);
+const storage = new Storage(client);
 const databaseID = process.env.APPWRITE_DATABASE_ID;
 const eventCollectionID = process.env.EVENT_COLLECTION_ID;
 const sessionCollectionID = process.env.SESSION_COLLECTION_ID;
+const bucketId = process.env.NEXT_PUBLIC_APPWRITE_BUCKET_ID;
+const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
 
 export {
   DB,
   ID,
   Query,
   account,
+  bucketId,
   databaseID,
   databases,
   eventCollectionID,
+  projectId,
   sessionCollectionID,
+  storage,
 };
