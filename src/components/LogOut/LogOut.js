77fb680784c1eb2994cf5afbb4082b73cc9959a2
Adrian Murage <adrianmurage21@gmail.com>
2023-06-13 16:27:05 +0300

    Add the landing page for the site
    - clean up the signin form
    - handle signed out and signed in states

--- src/components/LogOut/LogOut.js
+++ src/components/LogOut/LogOut.js
@@ -1,5 +1,5 @@
-import { UseUser } from "@/hooks/User";
-import Link from "next/link";
+import { UseUser } from '@/hooks/User';
+import Link from 'next/link';
 
 function LogOut() {
   const { user, loading, logout } = UseUser();
@@ -7,14 +7,16 @@ function LogOut() {
   // If user is not authenticated, display sign in button
   if (!loading && !user) {
     return (
-      <Link href="/auth/signin">
-        <button className="btn">Sign in</button>
+      <Link href="/">
+        <button className="btn inline-flex items-center px-4 py-2 mt-2 font-medium text-white transition duration-500 ease-in-out transform rounded-lg text-md md:mt-0 md:ml-4 bg-orange">
+          Sign in
+        </button>
       </Link>
     );
   }
   return (
     <div>
-      <button className="btn" onClick={logout}>
+      <button className="btn btn-outline  capitalize" onClick={logout}>
         Sign Out
       </button>
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,52 +1,13 @@
-import Link from 'next/link';
-import { databases, databaseID, eventCollectionID } from '../utils/appwrite';
-import EventCard from '../components/EventCard/EventCard';
-import LogOut from '@/components/LogOut/LogOut';
-import { UseUser } from '@/hooks/User';
-import { useRouter } from 'next/router';
+import Footer from '@/components/Footer/Footer';
+import Header from '@/components/Header/Header';
+import Main from '@/components/Main/Main';
 
-export default function Home({ eventList }) {
-  console.log(eventList);
-  const { user, loading } = UseUser();
-  const router = useRouter();
-  if (loading) return;
-  // Redirect unauthenticated users to signin page
-  if (!loading && !user) {
-    router.push('/auth/signin');
-    return;
-  }
+export default function Home() {
   return (
     <>
-      <LogOut />
-      <Link href="/newEvent">
-        <button className="btn">New Event</button>
-      </Link>
-      <div className="space-y-4">
-        {eventList.map((singleEvent) => (
-          <EventCard
-            key={singleEvent.$id}
-            eventName={singleEvent.name}
-            eventLink={`/event/${singleEvent.$id}`}
-          />
-        ))}
-      </div>
+      <Header />
+      <Main />
+      <Footer/>
     </>
   );
 }
-
-export const getServerSideProps = async () => {
-  try {
-    const results = await databases.listDocuments(
-      databaseID,
-      eventCollectionID
-    );
-
-    const eventList = results.documents;
-
-    return { props: { eventList } };
-  } catch (error) {
-    const eventList = error.message;
-
-    return { props: { eventList } };
-  }
-};
