06b13fef3e96ccd4b27ad0c7f5369709a3d2eadd
Adrian Murage <adrianmurage21@gmail.com>
2023-06-15 05:50:01 +0300

    landing page phone mockup

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PhoneMockup from '../PhoneMockup/PhoneMockup';
 
 export default function Main() {
   return (
@@ -22,12 +23,13 @@ export default function Main() {
             </a>
           </div>
         </div>
-        <div className="xl:mr-44 sm:mr-0 sm:mb-28 mb-0 lg:mb-0 mr-48 md:pl-10">
-          <img
+        <div className="">
+          {/* <img
             className="w-80 md:ml-1 ml-24"
             alt="iPhone-12"
             src="/images/iPhone-12-Mockup.png"
-          ></img>
+          ></img> */}
+          <PhoneMockup />
         </div>
       </div>
       <section className="mx-auto">
